35b749e2eaf913777414abf15d648e5a27662e61
manmohanjina <manish63singh@gmail.com>
2023-03-28 00:30:15 +0530

    todo crud complete

--- index.js
+++ index.js
@@ -11,11 +11,15 @@ app.use(cors())
 app.use(express.json());
 require("dotenv").config();
 
-app.use('/',testRoute)
+
 app.use("/", registerRouter);
 app.use("/", loginRoute);
 app.use('/',todoRoute)
 app.use('/',forgotRouter)
+
+app.get('/',async(req,res)=>{
+  res.send({"msg":"welcome"})
+})
 
 app.listen(process.env.port, () => {
   connection();
